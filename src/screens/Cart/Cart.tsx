9ee044d24a2e7339ab8207f974a273513e292539
Carlos Petit <ca.alberto.p@gmail.com>
2020-07-24 18:55:05 -0400

    Update price

--- src/screens/Cart/Cart.tsx
+++ src/screens/Cart/Cart.tsx
@@ -33,7 +33,14 @@ class Cart extends React.Component<HomeProps, State> {
         this.props.removeAction([...actualCart])
     }
 
-    handleRefresh = () => {
+    getTotal = () => {
+        let actualCart = [...this.props.cart.shopingBag]
+        let total = 0
+        actualCart.map((item) => {
+            total = total + item.amount
+        })
+
+        return total.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.") + " CLP"
     };
 
 
@@ -69,7 +76,7 @@ class Cart extends React.Component<HomeProps, State> {
                         <Text style={{ marginLeft: 20, fontSize: RFValue(19) }}>Bolsa de compra</Text>
                         <Text style={{ marginLeft: 20, fontSize: RFValue(19) }}>({this.props.cart.shopingBag.length} productos)</Text>
                     </View>
-                    <View style={{ flex: 5, justifyContent: 'center', alignItems: 'flex-start' }}>
+                    <View style={{ flex: 2, justifyContent: 'center', alignItems: 'flex-start' }}>
                         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
                             {
                                 this.props.cart.shopingBag.map((item, index) => {
@@ -84,11 +91,14 @@ class Cart extends React.Component<HomeProps, State> {
                                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                                                 <Text style={{ marginLeft: 20 }}>{item.amiiboSeries}</Text>
                                             </View>
+                                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                                <Text style={{ marginLeft: 20 }}>{item.amount.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.") + " CLP"}</Text>
+                                            </View>
                                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
                                                 <TouchableWithoutFeedback onPress={() => { this.removeFromCart(index) }} style={{}}>
                                                     <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                                                         <View style={{ justifyContent: 'center', alignItems: 'flex-end', marginRight: 5 }}>
-                                                            <Text>Remover</Text>
+                                                            {/* <Text>Remover</Text> */}
                                                         </View>
                                                         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end', marginRight: 20 }}>
                                                             <AntDesign name='closecircleo' size={24} color={"black"} />
@@ -104,7 +114,12 @@ class Cart extends React.Component<HomeProps, State> {
 
                     </View>
 
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
+                        <View style={{ height: hp(8), width: wp(100), justifyContent: 'center', alignItems: 'flex-end' }}>
+                            <Text style={{ fontSize: RFValue(18), color: '#333333', marginRight: 20 }}>{"Total: " + this.getTotal()}</Text>
+                        </View>
+                    </View>
+                    <View style={{ flex: 4, justifyContent: 'center', alignItems: 'center' }}>
                         <View style={{ backgroundColor: '#32CD32', height: hp(8), width: wp(45), borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
                             <Text style={{ fontSize: RFValue(18), color: '#333333' }}>{"Ir a comprar"}</Text>
                         </View>
